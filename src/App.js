522dfc01044c352b485ee077ee18361cd3054340
danchew90 <danchew90@naver.com>
2021-08-18 01:34:56 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("component rending"); // <- 최초에 랜딩이 된후에 동작
+  }
+  componentDidUpdate() {
+    console.log("component Update"); //<- component에 변화가 생길때 만 동작, 지금같은 경우에서는 setState가 생겨 랜딩을 다시 하기 때문에
+  }
   render() {
+    console.log("I'm rending");
     return (
       <div>
         <h1>The numver is: {this.state.count}</h1>
